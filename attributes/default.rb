143bd12858d03e1bc1b0856e01823199a6a0dc33
Melinda Moran <melinda.moran@johnmuirhealth.com>
2015-05-08 13:52:09 -0700

    inital changes for non_root user and merges from other cookbook pull requests

--- attributes/default.rb
+++ attributes/default.rb
@@ -16,12 +16,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+default['java']['jdk_version'] = 8
+default['java']['install_flavor'] = 'oracle'
+default['java']['oracle']['accept_oracle_download_terms'] = true
+# Hard coding for now, need to get found
+default['activemq']['java_home'] = '/usr/lib/jvm/jdk1.8.0_20'
+
+default['activemq']['usejmx'] = true
+default['activemq']['run_as_user'] = 'activemq'
 
 default['activemq']['mirror']  = 'https://repository.apache.org/content/repositories/releases/org/apache'
-default['activemq']['version'] = '5.9.1'
-default['activemq']['home']  = '/opt'
+default['activemq']['version'] = '5.11.0'
+default['activemq']['home']  = '/usr/local'
 default['activemq']['wrapper']['max_memory'] = '1024'
 default['activemq']['wrapper']['useDedicatedTaskRunner'] = 'true'
+default['activemq']['wrapper']['keystore_password'] = 'password'
+default['activemq']['wrapper']['truststore_password'] = 'password'
+default['activemq']['wrapper']['keystore_path'] = '%ACTIVEMQ_CONF%/broker.ks'
+default['activemq']['wrapper']['truststore_path'] = '%ACTIVEMQ_CONF%/broker.ts'
 
 default['activemq']['enable_stomp'] = true
-default['activemq']['use_default_config'] = false
+# default['activemq']['use_default_config'] = false
+
+#Properties related to 'activemq' 'broker'.
+default['activemq']['broker_name'] = 'localhost'
+
+#Admin console properties
+default['activemq']['admin_console']['customize'] = false
+default['activemq']['admin_console']['port'] = '8161'
+default['activemq']['admin_console']['credentials']['customize'] = true
+default['activemq']['admin_console']['credentials']['user_name'] = 'admin'
+default['activemq']['admin_console']['credentials']['password'] = 'admin'
+default['activemq']['admin_console']['new_admin_console_name'] = 'admin'
+
+
--- recipes/default.rb
+++ recipes/default.rb
@@ -18,32 +18,28 @@
 #
 
 include_recipe 'java::default'
+include_recipe 'ark'
 
 tmp = Chef::Config[:file_cache_path]
 version = node['activemq']['version']
 mirror = node['activemq']['mirror']
-activemq_home = "#{node['activemq']['home']}/apache-activemq-#{version}"
+activemq_home = "#{node['activemq']['home']}/apache-activemq"
 
-directory node['activemq']['home'] do
-  recursive true
+user node['activemq']['run_as_user'] do
+  comment "ActiveMQ user"
+  action :create
 end
 
-unless File.exists?("#{activemq_home}/bin/activemq")
-  remote_file "#{tmp}/apache-activemq-#{version}-bin.tar.gz" do
-    source "#{mirror}/activemq/apache-activemq/#{version}/apache-activemq-#{version}-bin.tar.gz"
-    mode   '0644'
-  end
-
-  execute "tar zxf #{tmp}/apache-activemq-#{version}-bin.tar.gz" do
-    cwd node['activemq']['home']
-  end
+ark "apache-activemq" do
+  url "#{mirror}/activemq/apache-activemq/#{version}/apache-activemq-#{version}-bin.tar.gz"
+  path node['activemq']['home']
+  version version
+  owner node['activemq']['run_as_user'] 
+  group node['activemq']['run_as_user']
+  action :install
+  not_if { File.exists?("#{activemq_home}/bin/activemq") }
 end
 
-file "#{activemq_home}/bin/activemq" do
-  owner 'root'
-  group 'root'
-  mode  '0755'
-end
 
 # TODO: make this more robust
 arch = node['kernel']['machine'] == 'x86_64' ? 'x86-64' : 'x86-32'
@@ -55,10 +51,40 @@ end
 template "#{activemq_home}/conf/activemq.xml" do
   source   'activemq.xml.erb'
   mode     '0755'
-  owner    'root'
-  group    'root'
+  owner    node['activemq']['run_as_user'] 
+  group    node['activemq']['run_as_user'] 
   notifies :restart, 'service[activemq]'
-  only_if  { node['activemq']['use_default_config'] }
+#  only_if  { node['activemq']['use_default_config'] }
+end
+
+template "#{activemq_home}/conf/jetty.xml" do
+  source   'jetty.xml.erb'
+  mode     '0755'
+  owner    node['activemq']['run_as_user']
+  group    node['activemq']['run_as_user']
+  notifies :restart, 'service[activemq]'
+  only_if  { node['activemq']['admin_console']['customize'] }
+end
+
+template "#{activemq_home}/conf/jetty-realm.properties" do
+  source   'jetty-realm.properties.erb'
+  mode     '0755'
+  owner    node['activemq']['run_as_user']
+  group    node['activemq']['run_as_user']
+  notifies :restart, 'service[activemq]'
+  only_if  { node['activemq']['admin_console']['credentials']['customize'] }
+end
+
+template '/etc/sysconfig/activemq' do
+  source 'sysconfig.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  variables( 
+      :run_as_user => node['activemq']['run_as_user'],
+      :java_home => node['activemq']['java_home']
+  )
+  notifies :restart, 'service[activemq]', :delayed
 end
 
 service 'activemq' do
@@ -80,5 +106,23 @@ end
 template "#{activemq_home}/bin/linux/wrapper.conf" do
   source   'wrapper.conf.erb'
   mode     '0644'
-  notifies :restart, 'service[activemq]'
+  notifies :restart, 'service[activemq]', :delayed
+end
+
+template "#{activemq_home}/conf/login.config" do
+  source 'login.config.erb'
+  mode    '0644'
+  notifies :restart, 'service[activemq]', :delayed
+end
+
+template "#{activemq_home}/conf/users.properties" do
+  source 'users.properties.erb'
+  mode    '0644'
+  notifies :restart, 'service[activemq]', :delayed
+end
+
+template "#{activemq_home}/conf/groups.properties" do
+  source 'groups.properties.erb'
+  mode    '0644'
+  notifies :restart, 'service[activemq]', :delayed
 end
